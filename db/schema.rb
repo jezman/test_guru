811d68dc6017ef02118bc0f6e9c640e5b76d3fa0
jezman <dev.jezman@gmail.com>
2018-10-04 16:04:05 +0300

    Remove user from answers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_071532) do
+ActiveRecord::Schema.define(version: 2018_10_04_130047) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id"
-    t.integer "user_id"
     t.boolean "correct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
