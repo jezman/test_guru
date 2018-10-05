0efc3d80c0c145a89f8bb32d5413d54a94583f78
jezman <dev.jezman@gmail.com>
2018-10-05 22:07:18 +0300

    Add  class method for Test.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :test_passings
   has_many :users, through: :test_passings
+
+  def self.tests_by_category(title)
+    join(:category).where(categories: { title: title })
+                   .order(title: :desc).pluck(:title)
+  end
 end
