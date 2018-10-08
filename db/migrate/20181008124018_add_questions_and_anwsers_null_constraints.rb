d7d66556ea5958ed288ce64b4379e621f68bf531
jezman <dev.jezman@gmail.com>
2018-10-08 15:42:35 +0300

    Add null contraints for question and answer.

---
+++ db/migrate/20181008124018_add_questions_and_anwsers_null_constraints.rb
@@ -0,0 +1,6 @@
+class AddQuestionsAndAnwsersNullConstraints < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :questions, :test_id, false
+    change_column_null :answers, :question_id, false
+  end
+end
