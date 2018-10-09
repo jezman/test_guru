4dd67e5d0aecb6cfecd04820081b05805d798e71
jezman <dev.jezman@gmail.com>
2018-10-09 13:41:45 +0300

    Fix formatting.

--- app/models/test.rb
+++ app/models/test.rb
@@ -14,13 +14,11 @@ class Test < ApplicationRecord
     greater_than_or_equal_to: 0
   }
 
-  scope :by_level, ->(level) { where level: level }
-  scope :easy, -> { by_level 0..1 }
-  scope :medium, -> { by_level 2..4 }
-  scope :hard, -> { by_level 5..Float::INFINITY }
-  scope :by_category, lambda { |title|
-    joins(:category).where(categories: { title: title })
-  }
+  scope :by_level, ->(level) { where(level: level) }
+  scope :easy, -> { by_level(0..1) }
+  scope :medium, -> { by_level(2..4) }
+  scope :hard, -> { by_level(5..Float::INFINITY) }
+  scope :by_category, ->(title) { joins(:category).where(categories: { title: title }) }
 
   def self.titles_by_category(title)
     by_category(title).order(title: :desc).pluck(:title)
