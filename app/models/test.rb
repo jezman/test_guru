06ec69c3cbce2f9515744129cf2239825bcd9730
jezman <dev.jezman@gmail.com>
2018-10-09 11:14:38 +0300

    Selects by difficulty level.

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,11 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passings
   has_many :questions
 
+  scope :by_level, ->(level) { where level: level }
+  scope :easy, -> { by_level 0..1 }
+  scope :medium, -> { by_level 2..4 }
+  scope :hard, -> { by_level 5..Float::INFINITY }
+
   def self.titles_by_category(title)
     join(:category).where(categories: { title: title })
                    .order(title: :desc).pluck(:title)
