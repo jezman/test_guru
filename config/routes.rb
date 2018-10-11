981089d41fc3dbaec174c988ddc732308802d539
jezman <dev.jezman@gmail.com>
2018-10-11 10:53:36 +0300

    Edit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tests#index'
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
+
+  get 'tests/:test_id/questions/:id', to: 'questions#show'
 end
