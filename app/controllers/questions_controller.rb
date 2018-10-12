836712c47e326755978bc58fccd775cedcaf82ab
jezman <dev.jezman@gmail.com>
2018-10-12 16:00:58 +0300

    Add questions views.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,39 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[create new]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    @questions = @test.questions
+  def new
+    @question = @test.questions.new
   end
 
-  def show
-    render plain: @question.body
-  end
+  def show; end
 
   def create
-    question = @test.questions.new(question_params)
+    @question = @test.questions.new(question_params)
 
-    if question.save
-      render plain: "Question '#{question_params[:body]}' created"
+    if @question.save
+      redirect_to test_path(@test)
     else
-      render plain: 'Creating error'
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to test_path(@question.test)
+    else
+      render :edit
     end
   end
 
   def destroy
-    question = @question.destroy
+    @question.destroy
 
-    render plain: "Question '#{question[:body]}' deleted"
+    redirect_to test_path(@question.test)
   end
 
   private
