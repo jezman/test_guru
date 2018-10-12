84d1afcfd7856c74b66f3ec6647bf547b4228eee
jezman <dev.jezman@gmail.com>
2018-10-12 16:00:47 +0300

    Add tests views.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,49 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
+
   def index
-    render plain: Test.all.inspect
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
