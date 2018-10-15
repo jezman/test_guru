29a17cc3dbae7c8b7b9c9d301ebf0928a2c4deea
jezman <dev.jezman@gmail.com>
2018-10-15 14:41:16 +0300

    Moved 'current_question' in to callback.

--- app/models/test_passing.rb
+++ app/models/test_passing.rb
@@ -3,12 +3,10 @@ class TestPassing < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question, on: %i[create update]
 
   def accept!(answer_ids)
     self.correct_answers += 1 if correct_answer?(answer_ids)
-
-    self.current_question = next_question
     save!
   end
 
@@ -18,8 +16,8 @@ class TestPassing < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
@@ -34,6 +32,6 @@ class TestPassing < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    test.questions.order(:id).where('id > ?', current_question.nil? ? 0 : current_question.id).first
   end
 end
