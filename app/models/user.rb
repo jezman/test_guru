18778c9be5854dafd79121edd0bb78e3494186ed
jezman <dev.jezman@gmail.com>
2018-10-16 11:58:10 +0300

    Add user authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   validates :email, presence: true
 
+  has_secure_password
+
   def tests_by_level(level)
     tests.by_level(level)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root to: 'tests#index'
+
+  get :signup, to: 'users#new'
+
+  resources :users, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
