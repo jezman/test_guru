f42ad2a07e1e25927dd2b20cc1de2541943d7a33
jezman <dev.jezman@gmail.com>
2018-10-16 14:36:59 +0300

    Validates user email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passings
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_secure_password
 
