16416a79160fe765e47d7cfc4c0b0c6425ffdbf9
jezman <dev.jezman@gmail.com>
2018-10-16 14:36:30 +0300

    Redirect user to request path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
+      cookies[:start_page] = request.url
       redirect_to login_path, alert: 'Are you a Guru? Verify your email and password please'
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to cookies[:start_page] || root_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your email and password please'
       render :new
