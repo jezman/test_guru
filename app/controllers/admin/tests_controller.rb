294c525093b200dc6da33f1a9c8b5dd45d55edd9
jezman <dev.jezman@gmail.com>
2018-10-17 17:06:07 +0300

    Set the test author to the administrator who created it.

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -14,7 +14,7 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def create
-    @test = Test.new(test_params)
+    @test = current_user.tests_author.new(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test)
@@ -49,6 +49,6 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 end
