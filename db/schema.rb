bebf6ae458494a2d6de57f1b8dcfe6c1c9478fde
jezman <dev.jezman@gmail.com>
2018-10-17 10:01:08 +0300

    Renamed username to name and added a unique index to email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_083157) do
+ActiveRecord::Schema.define(version: 2018_10_17_065616) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -61,10 +61,11 @@ ActiveRecord::Schema.define(version: 2018_10_16_083157) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "username"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
