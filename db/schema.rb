52695fe7d1d2185ea598f08d788f2e12da111383
jezman <dev.jezman@gmail.com>
2018-10-17 14:47:38 +0300

    Add admin model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_094852) do
+ActiveRecord::Schema.define(version: 2018_10_17_114224) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_094852) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "name"
+    t.string "first_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
@@ -77,9 +77,12 @@ ActiveRecord::Schema.define(version: 2018_10_17_094852) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
 end
