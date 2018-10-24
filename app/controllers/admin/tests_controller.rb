75107ac21517357a64acfbf52218d8e20f3e624d
jezman <dev.jezman@gmail.com>
2018-10-24 12:09:04 +0300

    Edit test title inline.

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,8 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy start update_inline]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def show; end
 
@@ -31,6 +30,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path(@test), notice: t('admin.updated', resource: @test.model_name.human)
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
 
@@ -38,6 +45,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def find_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
