8647cfd4330a2e8db6667a0d23229049dc73e615
jezman <dev.jezman@gmail.com>
2018-10-25 12:15:29 +0300

    Preparing for deployment.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_20_142920) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
