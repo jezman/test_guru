ac5db1283f8896c3eabbe0f1e4afc93b7f23ac6a
jezman <dev.jezman@gmail.com>
2018-10-26 08:33:57 +0300

    Fix.

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,17 +1,9 @@
 class FeedbackMailer < ApplicationMailer
-  before_action :find_admin
-
   def send_feedback(feedback)
     @user = feedback.user
     @subject = feedback.title
     @message = feedback.body
 
-    mail to: @admin.email, from: @user.email, subject: @subject
-  end
-
-  private
-
-  def find_admin
-    @admin = User.find_by(type: 'Admin')
+    mail to: Admin.first.email, from: @user.email, subject: @subject
   end
 end
