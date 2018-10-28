a6d69613ae0e98707f4bd4b75e5af6dd5fd5b63f
jezman <dev.jezman@gmail.com>
2018-10-28 09:58:00 +0300

    Update view and controller.

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -55,6 +55,6 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :time_limit, :category_id)
   end
 end
