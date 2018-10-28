78bb997f95a454126d84fa2bf156ba099ea615ec
jezman <dev.jezman@gmail.com>
2018-10-28 23:03:37 +0300

    Fix after review.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,17 +1,17 @@
 class Badge < ApplicationRecord
-  before_save :set_image_path
-
-  def self.images
-    image_path = 'app/assets/images/'
-    badges = Dir.glob("#{image_path}badges/*")
-    badges.map { |badge_path| badge_path.gsub(image_path, '') }
-  end
-
   BADGES_TYPE = {
     'Прохождение всех тестов категории' => :all_category_tests,
     'Прохождение теста с первой попытки' => :test_first_try,
     'Прохождение всех тестов определнного уровня' => :all_level_tests
   }.freeze
+
+  before_save :set_default_image_path, unless: :image_path?
+
+  def self.images
+    images_path = 'app/assets/images/'
+    badges = Dir.glob("#{images_path}badges/*")
+    badges.map { |badge_path| badge_path.gsub(images_path, '') }
+  end
 
   has_many :users_badges
   has_many :users, through: :users_badges
@@ -22,7 +22,7 @@ class Badge < ApplicationRecord
 
   private
 
-  def set_image_path
-    self.image_path = 'badges/badge-default.png' if image_path.blank?
+  def set_default_image_path
+    self.image_path = 'badges/badge-default.png'
   end
 end
